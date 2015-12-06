4e4484e00f72c5c19f93dc66af31ae151c1a5ef4
Maxim Tkachenko <maxim@2dehands.be>
2015-12-06 22:19:32 +0100

    Server's ip address sen be set via command line parameter.

--- server/org/home/server/home.py
+++ server/org/home/server/home.py
@@ -3,9 +3,8 @@ import org.home.common.log as log
 from org.home.server.request_handler import HomeRequestHandler
 import signal
 import threading
-
-IP_ADDRESS = '0.0.0.0'
-PORT_NUMBER = 8080
+import getopt
+import sys
 
 
 def shutdown_server(signal, frame):
@@ -13,11 +12,26 @@ def shutdown_server(signal, frame):
     threading.Thread(daemon=True, target=server.shutdown).start()
 
 
+def get_ip_from_args():
+    opts, args = getopt.getopt(sys.argv[1:], "a:")
+
+    for opt, arg in opts:
+        if opt == "-a":
+            return arg
+
+    return '0.0.0.0'
+
+IP_ADDRESS = get_ip_from_args()
+IP_PORT = 8080
+
 log.init()
 signal.signal(signal.SIGTERM, shutdown_server)
 
-server = HTTPServer((IP_ADDRESS, PORT_NUMBER), HomeRequestHandler)
-log.i('Started on port %s' % PORT_NUMBER)
+server = HTTPServer((IP_ADDRESS, IP_PORT), HomeRequestHandler)
+log.i('Started on {0}:{1}'.format(IP_ADDRESS, IP_PORT))
+
+if IP_ADDRESS == '0.0.0.0':
+    print('You can set an ip address by passing -a parameter: ... -a 127.0.0.1')
 
 try:
     # Wait forever for incoming http requests
